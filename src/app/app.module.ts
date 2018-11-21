276953a5c02f4f0cef59b6ad8e6558da64bd611d
alifrontend96 <alifrontend96@gmail.com>
2018-11-21 22:37:43 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+// ROUTING LINKS
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: '', component: SearchComponent, pathMatch: 'full' },
+  { path: '**', component: PagenotfoundComponent, pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
